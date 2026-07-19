0b21380b5ec4c462956e573da0052899353cf810
ts-31 <104221666+ts-31@users.noreply.github.com>
2026-07-19 21:38:57 +0530

    Fix backend 400 error and configure frontend API URL

--- server/config/settings.py
+++ server/config/settings.py
@@ -27,11 +27,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*,localhost,127.0.0.1', cast=Csv())
 
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
-    default="http://localhost:3000,http://127.0.0.1:3000",
+    default="http://localhost:3000,http://127.0.0.1:3000,https://herbal-udyog.vercel.app",
     cast=Csv()
 )
 CORS_ALLOW_CREDENTIALS = True
